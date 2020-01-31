9e62ca92a58c233b88012b9f868227730937fed3
haolipeng <1078285863@qq.com>
2020-02-01 07:49:12 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/tdd_study.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/tmp/tmp.II087IxZAP/SoundexTest.cpp" "/tmp/tmp.II087IxZAP/cmake-build-debug/CMakeFiles/tdd_study.dir/SoundexTest.cpp.o"
+  "/tmp/tmp.II087IxZAP/main.cpp" "/tmp/tmp.II087IxZAP/cmake-build-debug/CMakeFiles/tdd_study.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ cmake-build-debug/CMakeFiles/tdd_study.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tdd_study.dir/SoundexTest.cpp.o"
+  "CMakeFiles/tdd_study.dir/main.cpp.o"
+  "tdd_study.pdb"
+  "tdd_study"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/tdd_study.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
